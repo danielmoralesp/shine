bda33f82f06295e31dc25475bc2dc5e916d1deb9
danielmoralesp <daniel_moralesp@hotmail.com>
2017-11-21 14:07:37 -0500

    Extracting CustomerSearchComponent Into Its Own File

---
+++ app/javascript/CustomerSearchComponent/index.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+import { Http } from "@angular/http";
+
+var CustomerSearchComponent = Component({
+  // Same code as before...
+});
+
+export { CustomerSearchComponent };
--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -9,11 +9,12 @@
 ***/
 import "hello_angular/polyfills";
 
-import { Component, NgModule    } from "@angular/core";
-import { BrowserModule          } from "@angular/platform-browser";
-import { FormsModule            } from "@angular/forms";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { Http,HttpModule        } from "@angular/http";
+import { NgModule                } from "@angular/core";
+import { BrowserModule           } from "@angular/platform-browser";
+import { FormsModule             } from "@angular/forms";
+import { platformBrowserDynamic  } from "@angular/platform-browser-dynamic";
+import { HttpModule              } from "@angular/http";
+import { CustomerSearchComponent } from "CustomerSearchComponent";
 
 var RESULTS = [
   {
